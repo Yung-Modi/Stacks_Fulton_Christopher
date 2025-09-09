6451072bc2bc1319f8d447148e028e8b25ee5377
Yung-Modi <chrisreaper404@gmail.com>
2025-09-09 14:35:41 -0500

    end of Project

--- Stacks_Fulton_Christopher/Card.cs
+++ Stacks_Fulton_Christopher/Card.cs
@@ -8,5 +8,20 @@ namespace Stacks_Fulton_Christopher
 {
     internal class Card
     {
+        // property to get the number of the card
+        public string Number { get; }
+
+        // property to get the suit of the card
+        public string Suit { get; }
+
+        // constructor to initialize a new card object with specified number and suit
+        public Card(string numParam, string suitParam)
+        {
+            // assigns a parameter to number and suit
+            Number = numParam;
+            Suit = suitParam;
+        }
+
+
     }
 }
